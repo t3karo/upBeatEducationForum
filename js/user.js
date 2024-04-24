e0570110029b11aa57c6b555c0e676c627ed31b1
Anil <t3shan00@students.oamk.fi>
2024-04-24 12:11:28 +0300

    Hide Register menu when users are logged in

--- js/user.js
+++ js/user.js
@@ -11,3 +11,12 @@ if (user.isLoggedIn) {
     login_link_a.innerHTML = "Login"
     login_link_a.href="login.html"
 }
+
+const hide_register = document.getElementById('hide_register');
+var navMenu = document.getElementById('navbar-nav');
+
+if (user.isLoggedIn) {
+    hide_register.style.display = 'none';
+} else {
+    hide_register.style.display = 'inline-block';
+}
