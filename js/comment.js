56da8ecc594ecb5465ce0cf89131932c53f4b29a
Anil <t3shan00@students.oamk.fi>
2024-05-01 17:54:12 +0300

    update comment box and CSS

--- js/comment.js
+++ js/comment.js
@@ -1,29 +1,27 @@
-// const render_comment_field = (parent_element, post) => {
-//     const comment_textarea = parent_element.appendChild(document.createElement('textarea'));
-//     comment_textarea.addEventListener('keypress', (event) => {
-//         if (event.key === "Enter") {
-//             event.preventDefault();
-//             const comment_text = comment_textarea.value;
+    const comment_textarea = parent_element.appendChild(document.createElement('textarea'));
+    comment_textarea.addEventListener('keypress', (event) => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            const comment_text = comment_textarea.value;
 
-//             console.log("Posting comment for user ID:", user.id, " and post ID:", post.id);  // Debugging output
+            console.log("Posting comment for user ID:", user.id, " and post ID:", post.id);  // Debugging output
 
-//             if (!user.id || !post.id) {
-//                 alert("Missing user or post ID!");
-//                 return;  // Prevent further execution if IDs are missing
-//             }
+            if (!user.id || !post.id) {
+                alert("Missing user or post ID!");
+                return;  // Prevent further execution if IDs are missing
+            }
 
-//             const data = JSON.stringify({
-//                 comment: comment_text,
-//                 account_id: user.id,
-//                 post_id: post.id
-//             });
+            const data = JSON.stringify({
+                comment: comment_text,
+                account_id: user.id,
+                post_id: post.id
+            });
 
-//             posts.addComment(data).then(count => {
-//                 comment_textarea.value = '';
-//                 document.querySelector('p#comment' + post.id).innerHTML = "Comments " + count;
-//             }).catch(error => {
-//                 alert(error);
-//             });
-//         }
-//     });
-// };
+            posts.addComment(data).then(count => {
+                comment_textarea.value = '';
+                document.querySelector('p#comment' + post.id).innerHTML = "Comments " + count;
+            }).catch(error => {
+                alert(error);
+            });
+        }
+    });
